216df6083d785a04a7364d3cc634d4dcaeada940
BalderasLupita <135151348+BalderasLupita@users.noreply.github.com>
2024-08-20 10:08:00 -0600

    cambios litos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']  // Cambiar styleUrl a styleUrls
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'eurocolor-front';
+  filteredProductos: any[] = []; // Lista de productos filtrados
+  productos: any[] = [/* Lista de productos */]; // Lista completa de productos
+filteredProductosPigmentos: any;
+
+  constructor(private router: Router) {}
+
+  searchProduct(event: Event) {
+    const searchTerm = (event.target as HTMLInputElement).value.toLowerCase();
+
+    this.filteredProductos = this.productos.filter(producto =>
+      producto.title.toLowerCase().includes(searchTerm)
+    );
+
+    if (this.filteredProductos.length === 1) {
+      const selectedProduct = this.filteredProductos[0];
+      this.router.navigate(['/productos', selectedProduct.id]); // Redirige al producto seleccionado
+    }
+  }
 }
